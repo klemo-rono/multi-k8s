4880788a095d58f0a7a4a61be0e2da734a7ca3a7
_klemo <ronoclement86@gmail.com>
2020-02-26 15:26:01 +0000

    added deployment files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t rklemo/multi-client:latest -t rklemo/multi-client:$SHA -f /client/Dockerfile ./client 
-docker build -t rklemo/multi-server:latest -t rklemo/multi-server:$SHA -f /server/Dockerfile ./server
-docker build -t rklemo/multi-worker:latest -t rklemo/multi-worker:$SHA  -f /worker/Dockerfile  ./worker
+docker build -t rklemo/multi-client:latest -t rklemo/multi-client:$SHA -f ./client/Dockerfile ./client 
+docker build -t rklemo/multi-server:latest -t rklemo/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rklemo/multi-worker:latest -t rklemo/multi-worker:$SHA  -f ./worker/Dockerfile  ./worker
 docker push rklemo/multi-client:latest
 docker push rklemo/multi-worker:latest
 docker push rklemo/multi-server:latest
